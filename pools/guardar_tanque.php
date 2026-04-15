0348ab1f850bd559a4d58aa6b0a0c721bdbed117
ViCor44 <victor.a.correia@gmail.com>
2026-04-15 16:44:44 +0100

    Fix viewer Modbus access and preserve full controller IP on tank save

--- pools/guardar_tanque.php
+++ pools/guardar_tanque.php
@@ -15,7 +15,7 @@ $analysis = $_POST['requires_analysis'];
 $has_reject_counter = ($type === 'piscina' && isset($_POST['has_reject_counter'])) ? (int)$_POST['has_reject_counter'] : 0;
 $volume_m3 = ($type === 'piscina' && !empty($_POST['volume_m3'])) ? (float)$_POST['volume_m3'] : null;
 $has_controller = isset($_POST['has_controller']) ? (int)$_POST['has_controller'] : 0;
-$controller_ip = ($has_controller == 1 && !empty($_POST['controller_ip'])) ? $_POST['controller_ip'] : null;
+$controller_ip = ($has_controller == 1 && !empty($_POST['controller_ip'])) ? trim($_POST['controller_ip']) : null;
 
 // Se estiver a editar (vem um 'id' escondido no formulário)
 if (isset($_POST['id']) && !empty($_POST['id'])) {
@@ -26,7 +26,7 @@ if (isset($_POST['id']) && !empty($_POST['id'])) {
             has_controller = ?, controller_ip = ?, has_reject_counter = ?, volume_m3 = ?
         WHERE id = ?
     ");
-    $stmt->bind_param("ssiiiiisii", $name, $type, $water_freq, $hypo, $analysis, $has_controller, $controller_ip, $has_reject_counter, $volume_m3, $tank_id);
+    $stmt->bind_param("ssiiiisiii", $name, $type, $water_freq, $hypo, $analysis, $has_controller, $controller_ip, $has_reject_counter, $volume_m3, $tank_id);
 } 
 // Se estiver a criar um novo
 else {
@@ -34,7 +34,7 @@ else {
         INSERT INTO tanks (name, type, water_reading_frequency, uses_hypochlorite, requires_analysis, has_controller, controller_ip, has_reject_counter, volume_m3) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     ");
-    $stmt->bind_param("ssiiiiisii", $name, $type, $water_freq, $hypo, $analysis, $has_controller, $controller_ip, $has_reject_counter, $volume_m3);
+    $stmt->bind_param("ssiiiisii", $name, $type, $water_freq, $hypo, $analysis, $has_controller, $controller_ip, $has_reject_counter, $volume_m3);
 }
 
 if ($stmt->execute()) {
